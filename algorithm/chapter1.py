7a1c3dcafde3317b3aa96dd9626ffd1e6a8ab779
jeremyweitencent <jeremyweitencent@github.com>
2022-05-25 21:08:07 +0800

    add base_converter

--- algorithm/chapter1.py
+++ algorithm/chapter1.py
@@ -1,3 +1,5 @@
+import sys
+
 from data_struct import Stack
 
 
@@ -17,5 +19,23 @@ def divideBy2(decimalNumber):
     return binString
 
 
+# 10进制转任意进制(2-16)
+def baseConverter(decimal, base):
+    digits = "0123456789ABCDEF"
+    remainStack = Stack()
+
+    while decimal > 0:
+        remain = decimal % base
+        remainStack.push(remain)
+        decimal = decimal // base  # 除base向下取整
+
+    binString = ""
+    while not remainStack.is_empty():
+        binString += digits[remainStack.pop()]
+
+    return binString
+
+
 if __name__ == "__main__":
     print(divideBy2(64))
+    print(baseConverter(256, 16))
